12eb18e191c0333b5b83c7c0d6a5597dcdb257c0
Simen Daehlin <sdaehlin@luno.com>
2023-02-01 19:09:01 +0000

    Started working on getting data on quiz

--- src/pages/Quiz/Quiz.tsx
+++ src/pages/Quiz/Quiz.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getQuestions } from "../../api/api";
 import { Activity } from "../../api/api.model";
-
+import { formatQuizType } from "../../helpers/helpers";
 
 export const Quiz = () => {
   const { type } = useParams<{ type: string }>();
@@ -14,10 +14,14 @@ export const Quiz = () => {
   useEffect(() => {
     // Making another async function instead of make useEffect async.
     const currentQuiz = async () => {
-      const { name, heading, activities } = await getQuestions();
-      const
+      const { name, heading, activities } = (await getQuestions()) || {};
+
+      // Could be extracted to helper function to clean up readability of useEffect
+      const quiz = activities?.filter((activity) => activity.activity_name.toLowerCase() === formatQuizType(type ? type : ""));
+      if (quiz) setCurrentQuiz(quiz[0]);
     };
-  }, [currentQuiz]);
+    currentQuiz();
+  }, [currentQuiz, type]);
 
   return <div>{type}</div>;
 };
