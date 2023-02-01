7b49e6da91190f5f67c73e691599933aceaad600
Simen Daehlin <sdaehlin@luno.com>
2023-02-01 22:42:16 +0000

    Working on logic to seperate activities

--- src/pages/Quiz/Quiz.tsx
+++ src/pages/Quiz/Quiz.tsx
@@ -2,7 +2,9 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getQuestions } from "../../api/api";
 import { ActivityOne } from "../../components/ActivityOne/ActivityOne";
-import { Activity } from "../../models";
+import { ActivityTwo } from "../../components/ActivityTwo/ActivityTwo";
+import { formatQuizType } from "../../helpers/helpers";
+import { Activity } from "../../types";
 
 export const Quiz = () => {
   const { type } = useParams<{ type: string }>();
@@ -12,8 +14,10 @@ export const Quiz = () => {
     // Making another async function instead of make useEffect async.
     const getQuiz = async () => {
       const { activities } = (await getQuestions()) || {};
-
-      setCurrentQuiz(activities![0]);
+      console.log(formatQuizType(type!));
+      const activity = activities?.find((activity) => activity.activity_name.toLocaleLowerCase() === formatQuizType(type!));
+      console.log(activity);
+      setCurrentQuiz(activity!);
     };
     getQuiz();
   }, [type]);
@@ -21,7 +25,7 @@ export const Quiz = () => {
   return (
     <div>
       {currentQuiz && currentQuiz.activity_name === "Activity One" ? <ActivityOne quiz={currentQuiz} /> : null}
-      {currentQuiz && currentQuiz.activity_name === "Activity Two" ? <p>Render {type?.toUpperCase()}</p> : null}
+      {currentQuiz && currentQuiz.activity_name === "Activity Two" ? <ActivityTwo quiz={currentQuiz} /> : null}
     </div>
   );
 };
