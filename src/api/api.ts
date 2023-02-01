b1d7f1875376c7221c79af3a14f2d5c61f6579d3
Simen Daehlin <sdaehlin@luno.com>
2023-02-01 17:18:01 +0000

    Added getQuestions

--- src/api/api.ts
+++ src/api/api.ts
@@ -1,15 +1,17 @@
+import { isDev } from "../helpers/helpers";
 import { ApiResponse } from "./api.model";
 
 export const getQuestions = async (): Promise<ApiResponse | undefined> => {
   // Should fetch from secret but we are using hardcoded url here
+  // Setting up a proxyURL for CORS issues please README
   const url = `https://s3.eu-west-2.amazonaws.com/interview.mock.data/payload.json`;
   const proxyURL = `https://proxy.fairytales.dev/`;
 
   try {
-    const response = await fetch(url);
-    console.log(response);
-    if (response.ok) {
-      return await response.json();
-    }
-  } catch (error) {}
+    const response = await fetch(isDev() ? `${proxyURL}${url}` : url);
+    if (response.ok) return await response.json();
+  } catch (error) {
+    // Adding propper error handling for now we will just console log
+    console.error(error);
+  }
 };
