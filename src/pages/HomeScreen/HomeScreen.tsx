d9a03cfadfb316770c660eda886d8c6694635460
Simen Daehlin <sdaehlin@luno.com>
2023-02-01 17:18:17 +0000

    fetching questions for testing

--- src/pages/HomeScreen/HomeScreen.tsx
+++ src/pages/HomeScreen/HomeScreen.tsx
@@ -1,6 +1,11 @@
+import { useEffect } from "react";
+import { getQuestions } from "../../api/api";
 import styles from "./HomeScreen.module.css";
 
 export const HomeScreen = () => {
+  useEffect(() => {
+    getQuestions();
+  }, []);
   return (
     <div className={styles.main}>
       <h1>Cupa Quiz / Error Find</h1>
