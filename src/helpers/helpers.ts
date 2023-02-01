0decc59d1522d4af8336c76181ced75207d8136c
Simen Daehlin <sdaehlin@luno.com>
2023-02-01 19:08:47 +0000

    Cleaned up moved helpers

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,5 +1,9 @@
+import { Activity } from "../api/api.model";
+
 const development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 
 export const isDev = (): boolean => {
   return development;
 };
+
+export const formatQuizType = (quizType: string): string => quizType.split("_").join("");
