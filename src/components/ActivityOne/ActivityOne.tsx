d74dbdbcce85fecf7161bfcdb5827644f09585c1
Simen Daehlin <sdaehlin@luno.com>
2023-02-01 21:37:46 +0000

    Added activityOne

--- src/components/ActivityOne/ActivityOne.tsx
+++ src/components/ActivityOne/ActivityOne.tsx
@@ -1,24 +1,36 @@
 import { useState } from "react";
-import { isQuestionRoundGuard } from "../../helpers/helpers";
-import { Question, Quiz } from "../../models";
+import { Quiz } from "../../models";
+import { Question } from "../Question/Question";
+import styles from "./ActivityOne.module.css";
 
 export const ActivityOne = ({ quiz }: Quiz) => {
   const [quizState, setQuizState] = useState<{ question: number; answers: string[] }>({ question: 0, answers: [] });
 
   const choiceHandler = (choice: boolean) => {
-    let question: Question;
-    const currentQuestion = quiz?.questions[quizState.question];
-    if (isQuestionRoundGuard(currentQuestion)) {
-      question = currentQuestion.questions[0];
-    } else {
-      question = currentQuestion;
-    }
-    const result = question.is_correct === choice ? "correct" : "incorrect";
+    const result = quiz?.questions[quizState.question].is_correct === choice ? "correct" : "incorrect";
     setQuizState({
       question: quizState.question + 1,
       answers: [...quizState.answers, result],
     });
   };
-  
-  return ()
+
+  return (
+    <div>
+      {quiz && quizState.question < quiz.questions.length ? (
+        <div>
+          <h1 className={styles.title}>Activity One</h1>
+          <h2 className={styles.questionNum}>{`Q${quizState.question + 1}`}.</h2>
+          <Question questionText={quiz ? quiz.questions[quizState.question].stimulus : ""} />
+          <button className={styles.answerButton} onClick={() => choiceHandler(true)}>
+            CORRECT
+          </button>
+          <button className={styles.answerButton} onClick={() => choiceHandler(false)}>
+            INCORRECT
+          </button>
+        </div>
+      ) : (
+        <h2>Nothing here</h2>
+      )}
+    </div>
+  );
 };
