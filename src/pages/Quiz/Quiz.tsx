3822242106b61d93a9919ccc38e8aa0591acb808
Simen Daehlin <sdaehlin@luno.com>
2023-02-01 21:38:06 +0000

    Cleaned up quiz

--- src/pages/Quiz/Quiz.tsx
+++ src/pages/Quiz/Quiz.tsx
@@ -1,39 +1,26 @@
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { getQuestions } from "../../api/api";
+import { ActivityOne } from "../../components/ActivityOne/ActivityOne";
 import { Activity } from "../../models";
-import { formatQuizType } from "../../helpers/helpers";
 
 export const Quiz = () => {
   const { type } = useParams<{ type: string }>();
   const [currentQuiz, setCurrentQuiz] = useState<Activity | null>(null);
-  const [metaData, setMetaData] = useState({
-    quizName: "",
-    heading: "",
-  });
+
   useEffect(() => {
     // Making another async function instead of make useEffect async.
     const getQuiz = async () => {
-      const { name, heading, activities } = (await getQuestions()) || {};
+      const { activities } = (await getQuestions()) || {};
 
-      // Could be extracted to helper function to clean up readability of useEffect
-      const quiz = activities?.filter((activity) => {
-        return activity.activity_name.toLowerCase() === formatQuizType(type ? type : "");
-      });
-      if (quiz) setCurrentQuiz(quiz[0]);
-
-      // Could do some more if checks here and not infer that it never will be undefined, technically we would have
-      // thrown an error in the getQuestions  and return null instead so we can check for that here.
-
-      setMetaData({ quizName: name!, heading: heading! });
+      setCurrentQuiz(activities![0]);
     };
     getQuiz();
   }, [type]);
 
   return (
     <div>
-      {type}
-      {currentQuiz && currentQuiz.activity_name === "Activity One" ? <h2>Render {type}</h2> : null}
+      {currentQuiz && currentQuiz.activity_name === "Activity One" ? <ActivityOne quiz={currentQuiz} /> : null}
       {currentQuiz && currentQuiz.activity_name === "Activity Two" ? <p>Render {type?.toUpperCase()}</p> : null}
     </div>
   );
