fefb5683292c9faae9b681e05092255d85a714de
Simen Daehlin <sdaehlin@luno.com>
2023-02-02 13:21:46 +0000

    Fixed issue with className

--- src/components/Choose/Choose.tsx
+++ src/components/Choose/Choose.tsx
@@ -1,14 +1,15 @@
 import { Link } from "react-router-dom";
 import { ACTIVITY } from "../../helpers/enums";
 import styles from "./Choose.module.css";
+
 export const ChooseQuiz = () => {
   return (
     <>
       <div className={styles.parent}>
-        <Link to="/quiz/activity-one" className="upperCase">
+        <Link to="/quiz/activity-one" className="uppercase">
           {ACTIVITY.ONE}
         </Link>
-        <Link to="/quiz/activity-two" className="upperCase">
+        <Link to="/quiz/activity-two" className="uppercase">
           {ACTIVITY.TWO}
         </Link>
       </div>
