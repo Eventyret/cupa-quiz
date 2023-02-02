4ecd5319e098f5c8ca107ff3919b10f9378f2595
Simen Daehlin <sdaehlin@luno.com>
2023-02-02 09:19:44 +0000

    Extracted to helper function to clean effect clean and reusable

--- src/helpers/helpers.ts
+++ src/helpers/helpers.ts
@@ -1,4 +1,4 @@
-import { Activity, Question, QuestionRound } from "../types";
+import { Activity } from "../types";
 
 const development: boolean = !process.env.NODE_ENV || process.env.NODE_ENV === "development";
 
@@ -7,3 +7,8 @@ export const isDev = (): boolean => {
 };
 
 export const formatQuizType = (quizType: string): string => quizType.split("-").join("");
+
+export const extractActivity = (activities: Activity[], urlname: string): Activity | undefined =>
+  activities?.find((activity) => {
+    return activity.activity_name.split(" ")[1].toLowerCase() === urlname?.split("-")[1].toLowerCase();
+  });
